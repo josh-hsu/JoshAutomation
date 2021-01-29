8fa3efb17e2ddafdde2107d2265ff3b775b6eafe
Josh Hsu <alenbos0517@gmail.com>
2021-01-29 17:25:15 +0800

    gl20: claudia: add MH select support

--- app/src/main/java/com/mumu/android/joshautomation/scripts/claudia/ClaudiaRoutine.java
+++ app/src/main/java/com/mumu/android/joshautomation/scripts/claudia/ClaudiaRoutine.java
@@ -10,6 +10,7 @@ import com.mumu.libjoshgame.GameLibrary20;
 import com.mumu.libjoshgame.ScreenColor;
 import com.mumu.libjoshgame.ScreenCoord;
 import com.mumu.libjoshgame.ScreenPoint;
+import com.mumu.libjoshgame.service.DeviceScreen;
 
 import java.util.ArrayList;
 
@@ -82,35 +83,99 @@ public class ClaudiaRoutine {
             return -1;
     }
 
+    private void selectFriendWithMonsterHunter() throws InterruptedException, GameLibrary20.ScreenshotErrorException {
+        ArrayList<ArrayList<ScreenPoint>> friendMHArrays = new ArrayList<>();
+        int index = 0;
+        friendMHArrays.add(SPTList("pBattleFriend1"));
+        friendMHArrays.add(SPTList("pBattleFriend2"));
+        friendMHArrays.add(SPTList("pBattleFriend3"));
+        friendMHArrays.add(SPTList("pBattleFriend4"));
+        friendMHArrays.add(SPTList("pBattleFriend5"));
+        friendMHArrays.add(SPTList("pBattleFriend6"));
+        friendMHArrays.add(SPTList("pBattleFriend7"));
+
+        mGL.setScreenshotPolicy(DeviceScreen.POLICY_MANUAL);
+        mGL.requestRefresh();
+        for(ArrayList<ScreenPoint> friendPoint : friendMHArrays) {
+            index++;
+            if (mGL.colorsAre(friendPoint)) {
+                sendMessage("找到第" + index + "朋友有MH");
+                mGL.mouseClick(friendPoint.get(0).coord);
+                sleep(1000);
+                break;
+            }
+        }
+        mGL.setScreenshotPolicy(DeviceScreen.POLICY_DEFAULT);
+    }
+
+    private void detectMonsterHunter() throws InterruptedException, GameLibrary20.ScreenshotErrorException {
+        boolean p1HasMH = false;
+        sendMessage("偵測魔物獵人");
+        mGL.mouseClick(SPTList("pBattleMagic").get(0).coord);
+        sleep(800);
+        if (mGL.colorsAre(SPTList("pBattleMonsterHunter"))) {
+            sendMessage("有魔獸獵人");
+            mGL.mouseClick(SPTList("pBattleMonsterHunter").get(0).coord);
+            sleep(100);
+            mGL.mouseClick(SPTList("pBattleMonsterHunter").get(0).coord);
+            p1HasMH = true;
+        } else {
+            sendMessage("沒有魔獸獵人");
+        }
+
+        sendMessage("換對友");
+        sleep(500);
+        mGL.mouseClick(SCDList("cBattleMembers").get(3));
+        sleep(500);
+        mGL.mouseClick(SCDList("cBattleMembers").get(3));
+        sleep(500);
+        if (p1HasMH) {
+            mGL.mouseClick(SPTList("pBattleMagic").get(0).coord);
+            sleep(1000);
+        }
+        if (mGL.colorsAre(SPTList("pBattleMonsterHunter"))) {
+            sendMessage("有魔獸獵人");
+            mGL.mouseClick(SPTList("pBattleMonsterHunter").get(0).coord);
+        } else {
+            sendMessage("沒有魔獸獵人");
+            if (mGL.colorsAre(SPTList("pBattleCancelMagic")))
+                mGL.mouseClick(SPTList("pBattleCancelMagic").get(0).coord);
+        }
+        sleep(500);
+        mGL.mouseClick(SCDList("cBattleMembers").get(0));
+        sleep(500);
+    }
+
     /*
      * Handle ATTACK -> RESULT
      */
     public int battleRoutine(boolean autoAttack, boolean useMonsterHunter) throws InterruptedException, GameLibrary20.ScreenshotErrorException {
-        if (mGL.colorsAre(SPTList("pBattleMagic")) && useMonsterHunter) {
-            sendMessage("有魔法");
-            sleep(1000);
-            mGL.mouseClick(SPTList("pBattleMagic").get(0).coord);
-            sleep(1000);
-            if (mGL.colorsAre(SPTList("pBattleMonsterHunter"))) {
-                sendMessage("有魔獸獵人");
-                mGL.mouseClick(SPTList("pBattleMonsterHunter").get(0).coord);
-            } else {
-                sendMessage("沒有魔獸獵人, bye");
-                mGL.mouseClick(SPTList("pBattleMagic").get(0).coord);
-            }
-            sleep(1000);
-        }
+        boolean monsterHunterUsed = false;
+        sendMessage("戰鬥開始");
+        sleep(1000);
 
         sendMessage("開始亂點");
         while (!mGL.colorsAre(SPTList("pBattleResult"))) {
+            if (!monsterHunterUsed && useMonsterHunter && mGL.colorsAre(SPTList("pBattleBossGauge"))) {
+                sendMessage("BOSS出現");
+                sleep(1200);
+                detectMonsterHunter();
+                monsterHunterUsed = true;
+            }
             mGL.mouseClick(SPTList("pBattleAttack").get(0).coord);
-            sleep(250);
+            sleep(150);
             mGL.mouseClick(SCDList("cBattleSkills").get(0));
-            sleep(250);
+            sleep(150);
+            mGL.mouseClick(SPTList("pBattleAttack").get(0).coord);
+            sleep(150);
             mGL.mouseClick(SCDList("cBattleSkills").get(1));
-            sleep(250);
+            sleep(150);
+            mGL.mouseClick(SPTList("pBattleAttack").get(0).coord);
+            sleep(150);
             mGL.mouseClick(SCDList("cBattleSkills").get(2));
-            sleep(250);
+            sleep(150);
+            mGL.mouseClick(SPTList("pBattleAttack").get(0).coord);
+            sleep(150);
             mGL.mouseClick(SCDList("cBattleSkills").get(3));
         }
 
@@ -142,8 +207,7 @@ public class ClaudiaRoutine {
         sleep(2000);
 
         if (mGL.colorsAre(SPTList("pBattleFriend"))) {
-            sendMessage("選一號朋友");
-            mGL.mouseClick(SPTList("pBattleFriend").get(0).coord);
+            selectFriendWithMonsterHunter();
             sleep(1000);
         }
 
@@ -160,11 +224,15 @@ public class ClaudiaRoutine {
      */
     public int preBattle() throws InterruptedException, GameLibrary20.ScreenshotErrorException {
         int timeout = 10000; //10 seconds
-        int sleepInterval = 100; // 100 ms
+        int sleepInterval = 500; // 500 ms
         int retryCount = timeout / sleepInterval;
 
-        while (!mGL.colorsAre(SPTList("pBattleAttack")) && retryCount-- > 0) {
+        while (mGL.colorsAre(SPTList("pBattleGo")) && retryCount-- > 0) {
             mGL.mouseClick(SPTList("pBattleGo").get(0).coord);
+            sleep(sleepInterval);
+        }
+
+        while (!mGL.colorsAre(SPTList("pBattleAttack")) && retryCount-- > 0) {
             sleep(sleepInterval);
         }
 
